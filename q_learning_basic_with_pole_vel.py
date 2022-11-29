8c1b6f2d7b16809634ec0d063b02b1695dc378d0
Twigman <twigman@posteo.de>
2022-11-29 19:44:13 +0100

    added pole velocity as parameter

--- q_learning_basic_with_pole_vel.py
+++ q_learning_basic_with_pole_vel.py
@@ -25,7 +25,7 @@ Truncation: Episode length is greater than 500 (200 for v0)
 
 https://www.gymlibrary.dev/environments/classic_control/cart_pole/
 '''
-num_episodes = 10000
+num_episodes = 20000
 # Reward threshold
 max_steps_per_episode = 475
 
@@ -39,7 +39,7 @@ exploration_rate = 1
 max_exploration_rate = 1
 min_exploration_rate = 0.01
 # slows down exploration with every episode
-exploration_decay_rate = 0.001
+exploration_decay_rate = 0.0005
 # precision
 round_digits = 2
 index_factor = pow(10, round_digits)
@@ -58,8 +58,11 @@ def init_q_table():
     action_space_size = 2
     # (-.2095, .2095)
     state_space = 0.3 * 2
+    # (-2, 2)
+    vel_space = 2 * 2 
     state_space_size = int(state_space * index_factor)
-    q_table = np.zeros((state_space_size, action_space_size))
+    vel_space_size = int(vel_space * index_factor)
+    q_table = np.zeros((state_space_size, vel_space_size, action_space_size))
 
     return q_table
 
@@ -88,21 +91,20 @@ def print_avg_reward_per(num_of_episodes=1000):
 
 def simulation(exploration_rate):
     total_reward = 0
-    seed = 12
-
-    #env = gym.make('CartPole-v1', render_mode='human')
     env = gym.make('CartPole-v1')
-    #env.action_space.seed(seed)
     q_table = init_q_table()
-
-    
 
     for episode in range(num_episodes):
         print('* Episode ' + str(episode + 1) + ' started *')
         #observation = env.reset(seed=seed)[0]
         observation = env.reset()[0]
+        #observation = env.reset()
+        pole_angle = observation[2]
+        pole_vel = observation[3]
+
         # observe the pole angle
-        state = int(round(observation[2], round_digits) * index_factor)
+        pole_angle_state = int(round(pole_angle, round_digits) * index_factor)
+        pole_vel_state = int(round(pole_vel, round_digits) * index_factor)
         #print('init obs: ' + str(observation[2]) + ' -> ' + str(round(observation[2], round_digits)) + ' -> ' + str(int(round(observation[2], round_digits) * index_factor)))
         #print('init state: ' + str(state))
         total_reward = 0
@@ -115,7 +117,7 @@ def simulation(exploration_rate):
             if exploration_rate_threshold > exploration_rate:
                 # exploit-mode
                 # choose action with highest q value for the current state
-                action = np.argmax(q_table[state,:])
+                action = np.argmax(q_table[pole_angle_state, pole_vel_state,:])
             else:
                 # explore-mode
                 # choose random action
@@ -126,7 +128,8 @@ def simulation(exploration_rate):
             #print('action: ' + str(action))
             # execute action
             observation, reward, terminated, truncated, info = env.step(action)
-            new_state = int(round(observation[2], round_digits) * index_factor)
+            new_pole_angle_state = int(round(observation[2], round_digits) * index_factor)
+            new_pole_vel_state = int(round(observation[3], round_digits) * index_factor)
             #print('init obs: ' + str(observation[2]) + ' -> ' + str(round(observation[2], round_digits)) + ' -> ' + str(int(round(observation[2], round_digits) * index_factor)))
 
             #print('new_state: ' + str(new_state))
@@ -140,13 +143,14 @@ def simulation(exploration_rate):
             # update q table
             #pre = q_table[state, action]
 
-            q_table[state, action] = (1 - learning_rate) * q_table[state, action] + learning_rate * \
-                (reduced_reward + discount_rate * np.max(q_table[new_state, :]))
+            q_table[pole_angle_state, pole_vel_state, action] = (1 - learning_rate) * q_table[pole_angle_state, pole_vel_state, action] + learning_rate * \
+                (reduced_reward + discount_rate * np.max(q_table[new_pole_angle_state, new_pole_vel_state, :]))
 
             #print('value: ' + str(pre) + ' -> ' + str(q_table[state, action])) 
             #print('state: ' + str(state) + ' -> ' + str(new_state))
-            state = new_state
-            state_list.add(state)
+            pole_angle_state = new_pole_angle_state
+            pole_vel_state = new_pole_vel_state
+            #state_list.add(state)
 
             if terminated or truncated:
                 break
@@ -169,7 +173,7 @@ print_avg_reward_per(num_of_episodes=1000)
 print_q_table(q_table)
 print('states: ' + str(state_list))
 
-np.savetxt('q_table.txt', q_table)
+np.save('q_table_with_pole_vel.npy', q_table)
 
 
 
---
+++ q_learning_basic_with_pole_vel_agent_plays.py
@@ -0,0 +1,102 @@
+import numpy as np
+import gym
+from gym.utils.play import play
+
+'''
+Exercise for reinforcement learning
+
+Used Algorithm: Q-Learning
+
+Find optimal q* for every state-action pair (s, a).
+Updates q* using the bellman equation.
+
+--------------------------------------------------------
+
+Reward of +1 for every step
+Reward threshold: 475
+
+The episode ends if any one of the following occurs:
+
+Termination: Pole Angle is greater than ±12° (-.2095, .2095)
+Termination: Cart Position is greater than ±2.4 (center of the cart reaches the edge of the display)
+Truncation: Episode length is greater than 500 (200 for v0)
+
+https://www.gymlibrary.dev/environments/classic_control/cart_pole/
+'''
+num_episodes = 10000
+# Reward threshold
+max_steps_per_episode = 475
+
+# alpha
+learning_rate = 0.1
+# gamma
+discount_rate = 0.99
+
+# 100% exploration
+exploration_rate = 1
+max_exploration_rate = 1
+min_exploration_rate = 0.01
+# slows down exploration with every episode
+exploration_decay_rate = 0.001
+# precision
+round_digits = 2
+index_factor = pow(10, round_digits)
+
+rewards_all_episodes = []
+state_list = set()
+
+
+def print_q_table(q_table):
+    print('\n******** Q-Table ********')
+    print(q_table.shape)
+    direction_q_table = np.split(q_table, 2)
+    print('right:')
+    print(direction_q_table[0])
+    print('-------------------------------------')
+    #print(q_table)
+    print('left:')
+    print(direction_q_table[1])
+
+
+def play_agent():
+    total_reward = 0
+    env = gym.make('CartPole-v1', render_mode='human')
+    q_table = np.load('q_table_with_pole_vel.npy')
+    print_q_table(q_table)
+    
+    observation = env.reset()[0]
+
+    pole_angle = observation[2]
+    pole_vel = observation[3]
+
+    # observe the pole angle
+    pole_angle_state = int(round(pole_angle, round_digits) * index_factor)
+    pole_vel_state = int(round(pole_vel, round_digits) * index_factor)
+
+    for step in range(max_steps_per_episode):
+        action = np.argmax(q_table[pole_angle_state, pole_vel_state,:])
+        print('action: ' + str(action))
+
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        new_pole_angle_state = int(round(observation[2], round_digits) * index_factor)
+        new_pole_vel_state = int(round(observation[3], round_digits) * index_factor)
+            #print('init obs: ' + str(observation[2]) + ' -> ' + str(round(observation[2], round_digits)) + ' -> ' + str(int(round(observation[2], round_digits) * index_factor)))
+
+            #print('new_state: ' + str(new_state))
+
+            #print('value: ' + str(pre) + ' -> ' + str(q_table[state, action])) 
+        #print('state: ' + str(state) + ' -> ' + str(new_state))
+        pole_angle_state = new_pole_angle_state
+        pole_vel_state = new_pole_vel_state
+
+        if terminated or truncated:
+            break
+
+        total_reward += reward           
+
+    env.close()
+    return total_reward
+
+reward = play_agent()
+print('total reward: ' + str(reward))
